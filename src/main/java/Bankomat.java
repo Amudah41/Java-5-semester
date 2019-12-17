56bc887cdf0ff9cd9d2f574fe843df3184bbf650
aleksey <LeshaERmail.ru@gmail.com>
2019-12-17 10:49:55 +0300

    .

--- src/main/java/Bankomat.java
+++ src/main/java/Bankomat.java
@@ -1,16 +1,15 @@
 import java.util.*;
 
 public class Bankomat {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long sum;
         String result = scanner.next();
         try {
-            Long.parseLong(result);
+            sum = Long.parseLong(result);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Сумма  должна быть целым числом.");
         }
-        sum = Long.parseLong(result);
         if (sum <= 0) {
             System.out.print("Значение суммы должно быть положительным.");
             System.exit(0);
@@ -28,31 +27,37 @@ public class Bankomat {
                 if (Integer.parseInt(tmp) > 0) {
                     banknotes.add(Integer.parseInt(tmp));
 
-                } else {
+                }
+                else {
                     System.out.print("Значение банкнот должно быть положительным.");
                     System.exit(0);
                 }
-            } else {
+            }
+            else {
                 System.out.print("Значение банкнот должно быть целым.");
                 System.exit(0);
             }
         }
-        DeleteDuplicates(banknotes);
+        if (banknotes.size() == 0) {
+            System.out.print("Ведите значения банкнот.");
+            System.exit(0);
+        }
+        delete_duplicates(banknotes);
         long[] counts = new long[banknotes.size()];
-        for (int i = 0; i < banknotes.size()-1; i++){
+        for (int i = 0; i < banknotes.size()-1; i++) {
             counts[i] = 0;
         }
-        System.out.println(Exchange(banknotes, counts, 0, sum));
-        System.out.print("Количество комбинаций: "+ Combinations);
+        System.out.println(exchange(banknotes, counts, 0, sum));
+        System.out.print("Количество комбинаций: "+ combinations);
     }
 
-    public static void DeleteDuplicates(List<Integer> banknotes){
+    public static void delete_duplicates(List<Integer> banknotes) {
         Collections.sort(banknotes);
         int tmp;
         int size = banknotes.size();
-        for(int i = 1; i < size - 1; i++){
+        for(int i = 1; i < size - 1; i++) {
              tmp = banknotes.get(i - 1);
-            while (banknotes.get(i) == tmp && i < size-1) {
+             while (banknotes.get(i) == tmp && i < size-1) {
                 banknotes.remove(i);
                 size--;
             }
@@ -63,13 +68,13 @@ public class Bankomat {
     }
 
     public static String result = "";
-    public static long Combinations = 0;
+    public static long combinations = 0;
 
-    public static String Exchange(List<Integer> banknotes, long[] counts, int index, long sum) {
+    public static String exchange(List<Integer> banknotes, long[] counts, int index, long sum) {
         if (index >= banknotes.size()) {
-            Combinations++;
+            combinations++;
             for (int i = 0; i < banknotes.size(); i++) {
-                for(int j = 0; j < counts[i]; j++){
+                for(int j = 0; j < counts[i]; j++) {
                     result += banknotes.get(i) +" ";
                 }
             }
@@ -77,16 +82,15 @@ public class Bankomat {
             return result;
         }
         if (index == banknotes.size() - 1) {
-
             if (sum % banknotes.get(index) == 0) {
                 counts[index] = sum / banknotes.get(index);
-                Exchange(banknotes, counts, index + 1, 0);
+                exchange(banknotes, counts, index + 1, 0);
             }
         }
         else {
             for (int i = 0; i <= sum / banknotes.get(index); i++) {
                 counts[index] = i;
-                Exchange(banknotes, counts, index + 1, sum - banknotes.get(index) * i);
+                exchange(banknotes, counts, index + 1, sum - banknotes.get(index) * i);
             }
         }
         return result;
